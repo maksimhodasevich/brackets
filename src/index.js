a5949256af60c8ca2be81b250952f62c0eeba3dd
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2019-03-18 22:34:47 +0300

    fix: delete 100% crutch and come back to 85%(need to fix 3 tests)

--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
  module.exports = function brackets(str, bracketsConfig) {
   if(str.length % 2 == 0){
-    if(str == '|(|)' || str == '5555512575557777777555566667888888667661133833448441111222233333444442266666' || str == '8888877878887777777888888887777777887887788788887887777777788888888887788888'){
-      return false;
-    }
     var chars = str.split('');
     if(isRightBrackrts(chars) == true){
       return true;
